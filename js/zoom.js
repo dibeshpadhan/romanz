4e0d5c00d4f718068bbb898fb1ded789e3f03979
dibeshpadhan <dibeshpradhan@gmail.com>
2016-03-15 15:50:01 +0545

    Added files via upload

---
+++ js/zoom.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	jQuery('.zoom-gallery').zoom-gallery();	 
+	$('.zoom-gallery').magnificPopup({
+		alert();
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	})
+})(jQuery);
+
+// window.onload = function() {
+//     if (window.jQuery) {  
+//         // jQuery is loaded  
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
